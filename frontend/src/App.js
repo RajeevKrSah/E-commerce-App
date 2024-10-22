b9371b904231b6dea69e16b241e1c530a40dd31b
RajeevKrSah <rajeevjnvkne@gmail.com>
2024-10-22 15:29:11 +0530

    Routing setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
-
-import './App.css';
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      Frontend
-    </div>
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
   );
 }
 
